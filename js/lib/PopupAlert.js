45e71590742e788f87d7a89e381fc9ec95c913de
Marcel Meyer <23430036+marcel-b-meyer1987@users.noreply.github.com>
2026-02-22 23:20:14 +0100

    Continue work on PopupAlert element class

--- js/lib/PopupAlert.js
+++ js/lib/PopupAlert.js
@@ -1,17 +1,39 @@
-class PopupAlert extends HTMLDivElement {
+class PopupAlert extends HTMLElement {
 
     title;
     msg;
+    xButton;
+    okButton;
 
     constructor(title, msg) {
+
+        super();
         this.title = title;
         this.msg = msg;
 
         //generate Popover HTML element with "display: none" attribute
+
+        this.outerHTML = `
+            <dialog closedby="any" class="popup-alert">
+                <div class="title-bar">
+                    <span class="title">${title}</span>
+                    <span class="x-button">X</span>
+                </div>
+                <div class="msg-box">
+                    <span class="msg">${msg}</span>
+                    <div>
+                        <button>OK</button>
+                    </div>
+                </div>
+            </dialog>
+        `;
+
+        this.xButton = this.querySelector(".x-button");
+        this.okButton = this.querySelector(".msg button");  
     }
 
     show() {
-        this.showPopover();
+        document.appendChild(this);
     }
 
     
